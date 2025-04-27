138a0e73b51834f164f8c0e9891caba35e13ff36
fjmasis <102482854+fjmasis@users.noreply.github.com>
2025-04-26 21:32:16 -0600

    Add project files.

---
+++ Models/LibroModel.cs
@@ -0,0 +1,14 @@
+﻿namespace BiblioAPI.Models
+{
+    public class LibroModel
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Autor {  get; set; }
+        public string Editorial { get; set; }
+        public string ISBN { get; set; }
+        public int Anio { get; set; }
+        public string Categoria { get; set; }
+        public string Existencias { get; set; }
+    }
+}
